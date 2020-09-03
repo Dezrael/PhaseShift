6693e36f84aa5ba5bb1ef5e492312452848e68aa
Sergey Zuev <sergej31913@gmail.com>
2020-09-04 02:16:06 +0300

    Add movement scripts

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    [SerializeField] private float speed = 20f;
+    [SerializeField] private float jumpPower = 100f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+}
