7ac679cb94cd4ab7378e05510424306761bc5089
Sergey Zuev <sergej31913@gmail.com>
2020-09-18 02:36:08 +0300

    Add dragons

---
+++ Assets/Scripts/Objects/Fireball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float lifeTime = 5f;
+    private new Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = transform.right * speed;
+        StartCoroutine("DestroyOnTime");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator DestroyOnTime()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Obstacles/Dragon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    [SerializeField] private GameObject fireballPrefab;
+    [SerializeField] private float reloadTime;
+    [SerializeField] private Transform spawnPoint;
+
+    void Start()
+    {
+        StartCoroutine("CastFireball");
+    }
+
+    private IEnumerator CastFireball()
+    {
+        while(true)
+        {
+            Instantiate(fireballPrefab, spawnPoint.position, transform.rotation);
+            yield return new WaitForSeconds(reloadTime);
+        }
+    }
+}
